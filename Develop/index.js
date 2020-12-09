4e35fb4d1a990ebd6109f3babf0fc07ff5df4c3b
Lili Cohen <lili.cohen.tv@gmail.com>
2020-12-08 23:34:20 -0800

    adding questions to the prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,40 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-];
+// array of questions for user
+const questions = ([
+    //print to readme file sections?
+    {type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Describe your project',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Provide installation instructions:',
+      name: 'installation',
+    },
+        {type: 'input',
+      message: 'Add usage information',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Include contribution guidlines',
+      name: 'contribution',
+    },
+    {
+      type: 'input',
+      message: 'List your test instructions',
+      name: 'tests',
+    },
+    //choose license options from a list?
+
+]);
 
 // function to write README file
 function writeToFile(fileName, data) {
